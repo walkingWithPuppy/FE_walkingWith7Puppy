e1f74d0b1ba6e5a0a68a56cc908f2774d0a7f5f9
devjjin <sujink34@naver.com>
2023-05-06 19:27:51 +0900

    Feat: 게시글 이미지 파일 경로 수정

--- src/component/board/Post.jsx
+++ src/component/board/Post.jsx
@@ -8,10 +8,7 @@ const Post = () => {
     <PostWrapper>
       <Link to={PATH_URL.POST}>
         <Area>강서구</Area>
-        <Image
-          src="https://images.unsplash.com/photo-1587463272361-565200f82b33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHB1cHB5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-          alt="강아지이미지"
-        />
+        <Image src={'/assets/images/board/puppy1.jpg'} alt="puppy1" />
         <Info>
           <Title>같이 산책할 친구 구합니다</Title>
           <Content>
