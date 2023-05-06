23445c9c5cf31880b83367a35bdad4bbd1d4fc39
xxyeon129 <xxyeon129@gmail.com>
2023-05-07 00:54:25 +0900

    Design: fourth section UI 구현

--- src/component/home/HomeSection04.jsx
+++ src/component/home/HomeSection04.jsx
@@ -3,13 +3,27 @@ import styled from 'styled-components';
 const HomeSection04 = () => {
   return (
     <Section04>
-      <Button></Button>
+      <Button>우리 동네 강아지 친구, 지금 찾아보세요!</Button>
     </Section04>
   );
 };
 
-const Section04 = styled.div``;
+const Section04 = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 12rem;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 30rem;
+  background-color: #fbae03;
+  padding: 1rem;
+  border-radius: 3rem;
+
+  font-size: 1.5rem;
+  color: white;
+  font-weight: 600;
+`;
 
 export default HomeSection04;
