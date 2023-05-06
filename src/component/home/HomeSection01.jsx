6926a2b606c54bc93a24005b1be5eb91c03ad0f9
xxyeon129 <xxyeon129@gmail.com>
2023-05-07 01:04:39 +0900

    Design: first section UI 조정, fontawesome script 추가

--- src/component/home/HomeSection01.jsx
+++ src/component/home/HomeSection01.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import headerImg from '../../assets/HeaderImage.png';
 
 const HomeSection01 = () => {
+  const rightArrowIcon = <i className="fa-solid fa-chevron-right" />;
+
   return (
     <Section01>
       <TextContainer>
@@ -12,7 +14,7 @@ const HomeSection01 = () => {
           <b>우리동네 애견 산책 메이트</b> 찾기 서비스
         </Describe>
       </TextContainer>
-      <Button>WWP 산책메이트 찾기</Button>
+      <Button>WWP 산책메이트 찾기{rightArrowIcon}</Button>
     </Section01>
   );
 };
@@ -40,23 +42,27 @@ const TextContainer = styled.div`
 `;
 
 const Title = styled.h1`
-  font-size: 3rem;
+  font-size: 3.2rem;
   font-weight: bold;
 `;
 
 const Describe = styled.p`
   line-height: 150%;
+  font-size: 1.2rem;
 `;
 
 const Button = styled.button`
   background-color: white;
-  width: 12rem;
+  width: 16rem;
   padding: 0.5rem 1rem;
   border-radius: 3rem;
   color: #fbae03;
   background-color: white;
   font-weight: 600;
-  font-size: 1rem;
+  font-size: 1.2rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export default HomeSection01;
